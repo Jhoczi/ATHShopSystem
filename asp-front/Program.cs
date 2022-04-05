70c24f84cc1b88faaf143c8e60686b8ed4792f25
Jakub Hoczek <hoczek.jakub@gmail.com>
2022-04-05 13:34:58 +0200

    Add views, change css style.

--- asp-front/Program.cs
+++ asp-front/Program.cs
@@ -5,16 +5,19 @@ using asp_front.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var defaultConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var postgresConnectionString = builder.Configuration.GetConnectionString("Postgres");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
+    options.UseSqlite(defaultConnectionString));
+// builder.Services.AddDbContext<DataContext>(options =>
+//     options.UseNpgsql(postgreConnectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+await CreateDbIfNotExists(app);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -42,3 +45,19 @@ app.MapControllerRoute(
 app.MapRazorPages();
 
 app.Run();
+
+async Task CreateDbIfNotExists(IHost host)
+{
+    // await using var scope = host.Services.CreateAsyncScope();
+    // var services = scope.ServiceProvider;
+    // try
+    // {
+    //     var context = services.GetRequiredService<DataContext>();
+    //     await AppSeeder.Seed(context);
+    // }
+    // catch (Exception e)
+    // {
+    //     var logger = services.GetRequiredService<ILogger<Program>>();
+    //     logger.LogError(e, "An error occurred creating the DB.");
+    // }
+}
